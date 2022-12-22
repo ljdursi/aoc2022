f89eacc812cdf0251a2619136638081ca32d5deb
Jonathan Dursi <jonathan@dursi.ca>
2022-12-21 21:44:54 -0500

    Whitespace

--- 20_mixing/mixing.cpp
+++ 20_mixing/mixing.cpp
@@ -104,7 +104,6 @@ std::vector<long> get_inputs(std::ifstream &input) {
 }
 
 int main(int argc, char **argv) {
-
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
         return 1;
